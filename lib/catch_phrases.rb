1958915ca303c7eec6349ba6a67d81d4a593876d
Crystal T <ctalley.cat@gmail.com>
2020-09-22 21:03:25 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,7 +6,10 @@ end
 
 def toadstool
   puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+end 
   
-def  
+def link 
+  puts out ' It's Dangerous To Go Alone! Take This' 
+  
 
 end
