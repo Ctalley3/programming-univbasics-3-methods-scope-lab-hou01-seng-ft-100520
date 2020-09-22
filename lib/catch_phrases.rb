2eda452d7f47bc9a1f9eaa0ab53c79e617afb0b7
Crystal T <ctalley.cat@gmail.com>
2020-09-22 21:21:34 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,7 +13,10 @@ def link
   puts phrase = "It's Dangerous To Go Alone! Take This."
   
 def all_phrases
- status = all the other catch phrases 
- 
+ all phrases = all the other catch phrases 
+ puts phrase = "It's-a me, Mario!"
+ puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+ puts phrase = "It's Dangerous To Go Alone! Take This."
+end
 
 end
