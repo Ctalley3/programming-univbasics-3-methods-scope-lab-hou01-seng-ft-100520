96f266bbb8d3ea6738aeaefc95a3e2fdba74ec2a
Crystal T <ctalley.cat@gmail.com>
2020-09-22 21:14:39 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,8 +9,8 @@ def toadstool
 end 
   
 def link 
-  status = "It's Dangerous To Go Alone! Take This"
-  puts phrase = "It's Dangerous To Go Alone! Take This"
+  status = "It's Dangerous To Go Alone! Take This."
+  puts phrase = "It's Dangerous To Go Alone! Take This."
   
 
 
